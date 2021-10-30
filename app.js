c04a24cec2c794550c5c85b65014db25b5ba2724
Sherri Winslow <swinslowcoding@gmail.com>
2021-10-30 03:33:24 -0700

    app.js: added question object and const inqruier

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
-const generatePage = require('./src/page-template.js');
-// require const must go at top
-const fs = require('fs');
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-// const name = profileDataArgs[0];
-// const github = profileDataArgs[1];
-// shorten to:
-const [name, github] = profileDataArgs;
-// const generatePage = (userName, githubName) => {
-//   return `
-//     Name: ${userName}
-//     GitHub: ${githubName}
-//   `;
-// };
+const inquirer = require('inquirer');
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
 
-fs.writeFile('index.html', generatePage(name, github), err => {
-  if (err) throw err;
+// const pageHTML = generatePage(name, github);
 
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
+// fs.writeFile('./index.html', pageHTML, err => {
+//   if (err) throw err;
+
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
+
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
